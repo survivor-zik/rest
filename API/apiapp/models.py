9a78f2547dc1d2b9c8595800b5e06a8cf11cf05b
ramismajeed <ramismajeed@gmail.com>
2023-07-20 23:13:57 +0500

    User Added

--- API/apiapp/models.py
+++ API/apiapp/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.core.validators import MinLengthValidator,EmailValidator
+from datetime import datetime
 
 # Create your models here.
 class User(models.Model):
     username=models.CharField(primary_key=True,auto_created=False)
-    name = models.CharField(max_length=100,null=True)
-    email = models.EmailField(null=False)
+    name = models.CharField(max_length=100,null=True,validators=[MinLengthValidator(8)])
+    email = models.EmailField(null=False,validators=[EmailValidator()])
     password = models.CharField(max_length=100,null=False)
     age=models.IntegerField(null=True)
+    datejoined=models.DateTimeField(auto_created=True,default=datetime.now())
     
+    def __str__(self):
+        return self.name
     
-    
+
